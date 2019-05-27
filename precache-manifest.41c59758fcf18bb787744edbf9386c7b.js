5d0be331ecf51a1f7e3dcb797fdff3785d1c5bf9
AmirhosseinSrv <amirhossein.srv@gmail.com>
2019-05-27 18:06:04 +0430

    Updates

---
+++ precache-manifest.41c59758fcf18bb787744edbf9386c7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed549d821991bd3f922fd739979fbb31",
+    "url": "/First-React-Resume/index.html"
+  },
+  {
+    "revision": "66b0e3564b5bb3998178",
+    "url": "/First-React-Resume/static/css/main.6427604a.chunk.css"
+  },
+  {
+    "revision": "f44a96b40e87e785b92d",
+    "url": "/First-React-Resume/static/js/2.2b9bb896.chunk.js"
+  },
+  {
+    "revision": "66b0e3564b5bb3998178",
+    "url": "/First-React-Resume/static/js/main.c757053b.chunk.js"
+  },
+  {
+    "revision": "d272671c58e1278d12e3",
+    "url": "/First-React-Resume/static/js/runtime~main.511bb1f2.js"
+  }
+]);
